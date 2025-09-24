f1e43c40b725dac132968c1f3ad02fbcc053d690
Varutieru <151426935+Varutieru@users.noreply.github.com>
2025-09-24 21:23:08 +0700

    build test

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+export const Header: React.FC = () => {
+    return (
+        <div className="bg-[#ae0200] w-full h-[10vh] flex items-center justify-between px-[1.042vw]">
+
+        </div>
+    );
+}
+export default Header;
